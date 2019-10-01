c6cf131a940d500296ab8d3da131e891e24fc1f7
Scoopta <scoopta@scoopta.email>
2019-09-30 21:38:56 -0700

    Shift will no longer cause the search box to pull focus

--- src/wofi.c
+++ src/wofi.c
@@ -363,6 +363,8 @@ static gboolean key_press(GtkWidget* widget, GdkEvent* event, gpointer data) {
 			return TRUE;
 		}
 		break;
+	case GDK_KEY_Shift_L:case GDK_KEY_Shift_R:
+		break;
 	default:
 		if(!gtk_widget_has_focus(entry)) {
 			gtk_entry_grab_focus_without_selecting(GTK_ENTRY(entry));

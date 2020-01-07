d07622de3e9390426f22dc09c5f7863e711c2b80
Scoopta <scoopta@scoopta.email>
2020-01-06 16:42:52 -0800

    Copyright update

--- inc/wofi.h
+++ inc/wofi.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2019 Scoopta
+ *  Copyright (C) 2019-2020 Scoopta
  *  This file is part of Wofi
  *  Wofi is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by

1266717492f95e162b0b640abe0f0dc6b17dcbbe
Scoopta <scoopta@scoopta.email>
2020-05-08 14:24:51 -0700

    Added 1px to the total size to get rid of the slightly undersized window when using --lines

--- src/wofi.c
+++ src/wofi.c
@@ -471,6 +471,7 @@ static void widget_allocate(GtkWidget* widget, GdkRectangle* allocation, gpointe
 	}
 	if(lines > 0) {
 		height = max_height * lines;
+		++height;
 		update_surface_size();
 	}
 }
